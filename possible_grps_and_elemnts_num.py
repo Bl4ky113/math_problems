d6a3dc8dd8de9dbf89461c655ba0a290c3a79370
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2022-02-26 19:22:35 -0500

    Added graphs possible_groups_and_elements_number

---
+++ possible_grps_and_elemnts_num.py
@@ -0,0 +1,27 @@
+from num_possible_groups import find_possible_groups
+import plotly.express as px
+
+def main ():
+    DATA_RANGE = 100
+    len_possible_groups = []
+    len_element_arr = []
+    num_per_group = 2
+
+    for i in range(DATA_RANGE):
+        element_arr = [j + 1 for j in range((num_per_group + 1) + i)]
+        possible_groups_arr = []
+
+        find_possible_groups(num_per_group, element_arr, possible_groups_arr)
+
+        len_possible_groups.append(len(possible_groups_arr))
+        len_element_arr.append(len(element_arr))
+
+    fig = px.scatter(
+        x=len_element_arr,
+        y=len_possible_groups
+    )
+
+    fig.show()
+
+if __name__ == "__main__":
+    main()
