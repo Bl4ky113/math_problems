cf473d8c78c2aece98105003e7b0bac6bc5f9ee2
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2022-03-03 19:46:59 -0500

    Added calc_date for days and hours function

--- medicines_each_hour.py
+++ medicines_each_hour.py
@@ -2,17 +2,17 @@ medicines = [
     {
         "grams": 500,
         "units": 30,
-        "each_hour": 3
-    },
-    {
-        "grams": 500,
-        "units": 30,
-        "each_hour": 4
-    },
-    {
-        "grams": 500,
-        "units": 30,
         "each_hour": 8
+    },
+    {
+        "grams": 500,
+        "units": 30,
+        "each_hour": 10
+    },
+    {
+        "grams": 500,
+        "units": 30,
+        "each_hour": 6
     }
 ]
 
@@ -25,6 +25,17 @@ name_days = [
     "sat",
     "sun"
 ]
+
+def calc_date(current_date=1, range_date=1, restart_count=24):
+    date = current_date
+
+    for i in range(range_date):
+        if date > restart_count:
+            date = 0
+
+        date += 1
+
+    return date
 
 def main():
     current_day_index =  4 - 1
@@ -52,9 +63,12 @@ def main():
         if iterations == 1000 or all_true:
             break
 
-    days_passed = iterations // 24 
-    hours_passed = ((iterations / 24) - (days_passed)) / 24
+    days_passed = iterations // 24
+    hours_passed = iterations % 24
 
+    hours_date = calc_date(current_hour, hours_passed, 24)
+    index_day_date = calc_date(current_day_index, days_passed, 6)
+    
     for i in range(int(hours_passed)):
         if current_hour == 24:
             current_hour = 0
@@ -69,8 +83,7 @@ def main():
 
         current_day_index += 1
 
-    print(medicines_count) 
-    print(f"In {days_passed} days and {hours_passed} hours on {name_days[current_day_index + 1]} {current_hour}, {person} will take the {len(medicines)} medicines")
+    print(f"In {days_passed} days and {hours_passed} hours on {name_days[index_day_date]} {hours_date}, {person} will take the {len(medicines)} medicines")
 
 if __name__ == "__main__":
     main()
